f8e6e8fa1b8e526bc6c41d97043c13e6ef9654a9
Illia <icrosil@gmail.com>
2015-06-03 01:04:09 +0300

    CUDA *12 improvements

--- 4.2/cuda.cu
+++ 4.2/cuda.cu
@@ -31,6 +31,7 @@
 #include <string>
 #include <ctime>
 #include <mkl.h>
+#include <stdio.h>
 
 using namespace std;
 using namespace alglib;
@@ -269,20 +270,29 @@ void Shablon(vector<vector<double> > X, double * &res) {
  #endif
 
 __device__ int barrier = N - 2;
-
-__global__ void mykernel (double *a, double *b, double c, double *d, int n) {
-//B, Shablon, Tau[i - 1], firstAppr
+__device__ int blocks = N - 2;
+__global__ void myshab (double *temp, int n, double *all) {
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    int lindex = index + N + 1 + 2 * (int) (index / (N - 2));
+        if (index < n) {
+            temp[index] = -4 * all[lindex] + all[lindex - N] + all[lindex + N] + all[lindex - 1] + all[lindex + 1];
+        }
+}
+__global__ void mykernel (double *a, double *b, double *c, double *d, int n, int i, double *all) {
+//B, Shablon, Tau, firstAppr, iteration number
 //tempAppr[j] = (B[j] - aMulX(A, firstAppr, j)) * Tau[i - 1] + firstAppr[j];
     //TODO syncthreads will work, so pass needed elements, let them for and sync on every iteration
     int index = threadIdx.x + blockIdx.x * blockDim.x;
+    int lindex = index + N + 1 + 2 * (int) (index / (N - 2));
     if (index < n) {
-        d[index] = (-a[index] + b[index]) * (c) + d[index];
+        d[index] = (-a[index] + b[index]) * c[i] + d[index];
+        all[lindex] = d[index];
     }
     // if i ever need an block sync (time expensive), or create own block sync with a vector of 00,
     // fill them with 1 when block is done and go next if all 1
     /* Do whatever it is that this block does. */
 
-    
+
    //  /* Make sure all threads in this block are actually here. */
    //  __syncthreads();
    //  /* Once we're done, decrease the value of the barrier. */
@@ -362,14 +372,17 @@ int main() {
     *main loop here
     */
     double *temp = new double [N * N - 4 * N + 4];
+    double *all = new double [N * N];
     double *b = new double [N * N - 4 * N + 4];
     double *fa = new double [N * N - 4 * N + 4];
-    double *d_a, *d_b, *d_d;
+    double *taum = new double [maxIter + 1];
+    double *d_a, *d_b, *d_c, *d_d, *d_g;
     int size = sizeof(double);
 
     cudaMalloc((void **)&d_a, size * (N * N - 4 * N + 4));
     cudaMalloc((void **)&d_b, size * (N * N - 4 * N + 4));
-    // cudaMalloc((void **)&d_c, size);
+    cudaMalloc((void **)&d_g, size * (N * N));
+    cudaMalloc((void **)&d_c, size * (maxIter + 1));
     cudaMalloc((void **)&d_d, size * (N * N - 4 * N + 4));
 
     for (int j = 1; j < N - 1; j++) {
@@ -379,28 +392,53 @@ int main() {
             fa[(j - 1) * (N - 2) + (k - 1)] = firstAppr[j][k];
         }
     }
+    for (int j = 0; j < N ; j++) {
+        for (int k = 0; k < N; k++) {
+            all[j * N + k] = firstAppr[j][k];
+        }
+    }
+    for (int i = 0; i < maxIter + 1; i++) {
+        taum[i] = Tau[i];
+    }
     cudaMemcpy(d_a, b, size * (N * N - 4 * N + 4), cudaMemcpyHostToDevice);
     cudaMemcpy(d_d, fa, size * (N * N - 4 * N + 4), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_c, taum, size * (maxIter + 1), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_g, all, size * (N * N), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, temp, size * (N * N - 4 * N + 4), cudaMemcpyHostToDevice);
     double timeChecker = dsecnd();
+    // char aster;
     for (int i = 1; i < maxIter + 1; ++i) {
-        cout<<"The "<<i<<" iter"<<endl;
-        Shablon(firstAppr, temp);
+        // cout<<"The "<<i<<" iter"<<endl;
+        myshab<<<N - 2, N - 2>>>(d_b, N * N - 4 * N + 4, d_g);
+        // cudaMemcpy(temp, d_b, size * (N * N - 4 * N + 4), cudaMemcpyDeviceToHost);
+        // cout<<endl<<"The temp from GPU is"<<endl;
+        // outVector(temp, N * N - 4 * N + 4);
+        // Shablon(firstAppr, temp);
         // cout<<"The temp is"<<endl;
         // outVector(temp, N * N - 4 * N + 4);
-        // cout<<endl<<Tau[i]<<" "<<&Tau[i];
-        // cudaMemcpy(d_c, &Tau[i], size, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_b, temp, size * (N * N - 4 * N + 4), cudaMemcpyHostToDevice);
-        mykernel<<<N - 2, N - 2>>>(d_a, d_b, Tau[i], d_d, N * N - 4 * N + 4);
-        cudaMemcpy(fa, d_d, size * (N * N - 4 * N + 4), cudaMemcpyDeviceToHost);
-        for (int j = 1; j < N - 1; j++) {
-            for (int k = 1; k < N - 1; k++) {
-                firstAppr[j][k] = fa[(j - 1) * (N - 2) + (k - 1)];
-            }
-        }
-        outMatr(firstAppr);
-        cout<<endl;
+        // cin>>aster;
+        // cudaMemcpy(d_b, temp, size * (N * N - 4 * N + 4), cudaMemcpyHostToDevice);
+        mykernel<<<N - 2, N - 2>>>(d_a, d_b, d_c, d_d, N * N - 4 * N + 4, i, d_g);
+        // cudaMemcpy(fa, d_d, size * (N * N - 4 * N + 4), cudaMemcpyDeviceToHost);
+        // cudaMemcpy(all, d_g, size * (N * N), cudaMemcpyDeviceToHost);
+        // for (int j = 1; j < N - 1; j++) {
+        //     for (int k = 1; k < N - 1; k++) {
+        //         firstAppr[j][k] = fa[(j - 1) * (N - 2) + (k - 1)];
+        //     }
+        // }
+        // cout<<endl<<"fa"<<endl;
+        // outMatr(firstAppr);
+        // cout<<"ALLL"<<endl;
+        // outVector(all, N * N);
+        // cout<<endl;
     }
     double tMain = dsecnd() - timeChecker;
+    cudaMemcpy(fa, d_d, size * (N * N - 4 * N + 4), cudaMemcpyDeviceToHost);
+    for (int j = 1; j < N - 1; j++) {
+        for (int k = 1; k < N - 1; k++) {
+            firstAppr[j][k] = fa[(j - 1) * (N - 2) + (k - 1)];
+        }
+    }
 
     // for (int i = 0; i < firstAppr.size(); i++) {
     //     firstAppr[i] /= ((firstAppr.size() - 1) * (firstAppr.size() - 1));
